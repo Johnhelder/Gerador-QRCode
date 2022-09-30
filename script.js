930fce692be7affd35adc29b98fdfcc346d9f0f4
John Helder <johnhelderjp@gmail.com>
2022-09-29 22:06:17 -0300

    Foi adicionado a função o link da API

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const btnValue = document.querySelector("#btnValue");
 const imgQrCode = document.querySelector("imgQrCode");
 const wrapper = document.querySelector(".wrapper");
 let valueDefault;
-//https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example 
+ 
 
 btnValue.addEventListener("click", ()=>{
   let qrcodeValue = inputValue.valeu.trim();
   if(!qrcodeValue || qrcodeValue === valueDefault) return;
   valueDefault = qrcodeValue;
   btnValue.innerText = "Gerando QR Code...";
+imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}`
+imgQrCode.addEventListener("load", () => {
+  wrapper.classList.add("active")
+})
 })
